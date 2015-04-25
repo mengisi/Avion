e5f6cf6dd1ff2610a344bd767fc5164cf72e972b
Carolina <carolinach@me.com>
2015-04-25 17:30:37 -0400

    Resume of ticket buy DONE, working 100%.
    Resume of ticket buy DONE, working 100%.

--- Avion/Avion/js/main2.js
+++ Avion/Avion/js/main2.js
@@ -11,30 +11,21 @@ window.onload = function () {
     var date_return_b=getParameterByName('date_return');
     var persons_b=getParameterByName('persons');
     var prix_total_b=getParameterByName('prix_total');
+    document.getElementById('lf').value =city_depart_b;
+	console.log(city_depart_b);
+	document.getElementById('ft').value =city_destine_b;
+	console.log(city_destine_b);
+	document.getElementById('dd').value =date_depart_b;
+	console.log(date_depart_b);
+	document.getElementById('da').value =date_return_b;
+	console.log(date_return_b);
+	document.getElementById('np').value =persons_b;
+	console.log(persons_b);
+	document.getElementById('per').value =prix_total_b;
+	console.log(prix_total_b);
 };
 
-function give_values(){
-	var city_depart_b=getParameterByName('city_depart');
-    var city_destine_b=getParameterByName('city_destine');
-    var date_depart_b=getParameterByName('date_depart');
-    var date_return_b=getParameterByName('date_return');
-    var persons_b=getParameterByName('persons');
-    var prix_total_b=getParameterByName('prix_total');
-	
-	document.getElementById('lf').innerHTML =city_depart_b;
-	console.log(city_depart_b);
-	document.getElementById('ft').innerHTML =city_destine_b;
-	console.log(city_destine_b);
-	document.getElementById('dd').innerHTML =date_depart_b;
-	console.log(date_depart_b);
-	document.getElementById('dr').innerHTML =date_return_b;
-	console.log(date_return_b);
-	document.getElementById('np').innerHTML =persons_b;
-	console.log(persons_b);
-	document.getElementById('per').innerHTML =prix_total_b;
-	console.log(prix_total_b);
-	
-}
+
 
 
 
