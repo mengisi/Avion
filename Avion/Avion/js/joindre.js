52e7ce2c09ceb66b16548c7f06c47ffc5489380f
Carolina <carolinach@me.com>
2015-04-26 11:40:57 -0400

    MENG COMMIT
    nos joindre page

---
+++ Avion/Avion/js/joindre.js
@@ -0,0 +1,15 @@
+'use strict';
+window.onload=initialiser();
+    function initialiser() {
+        var latlng = new google.maps.LatLng(45.5440916, -73.6402476);
+        var options = {
+            center: latlng,
+            zoom: 17,
+            mapTypeId: google.maps.MapTypeId.ROADMAP
+        };
+        var carte = new google.maps.Map(document.getElementById("carte"), options);
+        var marqueur = new google.maps.Marker({
+            position: new google.maps.LatLng(45.5440916, -73.6402476),
+            map: carte
+        });
+    }
